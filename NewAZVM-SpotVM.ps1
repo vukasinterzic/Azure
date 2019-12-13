1e1623a8e1d3d67242de3b58539542002ba92d9a
Vukasin Terzic <vukasin.terzic@outlook.com>
2019-12-13 10:17:51 +0100

    new SpotVM

---
+++ NewAZVM-SpotVM.ps1
@@ -0,0 +1,43 @@
+
+<#
+Using Spot VMs allows you to take advantage of our unused capacity at a significant cost savings. 
+At any point in time when Azure needs the capacity back, the Azure infrastructure will evict Spot VMs. 
+Therefore, Spot VMs are great for workloads that can handle interruptions like batch processing jobs, dev/test environments, large compute workloads and more.
+
+You have option to set a max price you are willing to pay, per hour, for the VM.
+#>
+
+
+
+$resourceGroup = "mySpotRG"
+$location = "eastus"
+$vmName = "mySpotVM"
+$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+New-AzResourceGroup -Name $resourceGroup -Location $location
+$subnetConfig = New-AzVirtualNetworkSubnetConfig `
+   -Name mySubnet -AddressPrefix 192.168.1.0/24
+$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroup `
+   -Location $location -Name MYvNET -AddressPrefix 192.168.0.0/16 `
+   -Subnet $subnetConfig
+$pip = New-AzPublicIpAddress -ResourceGroupName $resourceGroup -Location $location `
+  -Name "mypublicdns$(Get-Random)" -AllocationMethod Static -IdleTimeoutInMinutes 4
+$nsgRuleRDP = New-AzNetworkSecurityRuleConfig -Name myNetworkSecurityGroupRuleRDP  -Protocol Tcp `
+  -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * `
+  -DestinationPortRange 3389 -Access Allow
+$nsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroup -Location $location `
+  -Name myNetworkSecurityGroup -SecurityRules $nsgRuleRDP
+$nic = New-AzNetworkInterface -Name myNic -ResourceGroupName $resourceGroup -Location $location `
+  -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+
+# Create a virtual machine configuration and set this to be a Spot VM
+
+$vmConfig = New-AzVMConfig -VMName $vmName -VMSize Standard_D1 -Priority "Spot" -MaxPrice -1| `
+Set-AzVMOperatingSystem -Windows -ComputerName $vmName -Credential $cred | `
+Set-AzVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2016-Datacenter -Version latest | `
+Add-AzVMNetworkInterface -Id $nic.Id
+
+New-AzVM -ResourceGroupName $resourceGroup -Location $location -VM $vmConfig
+
+#After the VM is created, you can query to see the max price for all of the VMs in the resource group.
+Get-AzVM -ResourceGroupName $resourceGroup | `
+   Select-Object Name,@{Name="maxPrice"; Expression={$_.BillingProfile.MaxPrice}}
