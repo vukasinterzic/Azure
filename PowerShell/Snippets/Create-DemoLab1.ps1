681c52818c0798fbe34ccfc3849bc7d087eb5e48
vukasinterzic <vukasin.terzic@outlook.com>
2022-01-16 10:21:58 -0800

    file added

---
+++ PowerShell/Snippets/Create-DemoLab1.ps1
@@ -0,0 +1,10 @@
+#Create a RG
+#Create a Virtual Network
+#Create a Subnet
+#Create a Public IP Address
+#Create a Virtual Machine x2 with the above created RG, Vnet, Subnet, Public IP Address
+
+
+#Create a Resource Group
+
+
