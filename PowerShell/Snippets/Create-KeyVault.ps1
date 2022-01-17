13ef980acddc18176a1bd32b9582350ef6819b2d
vukasinterzic <vukasin.terzic@outlook.com>
2022-01-17 14:15:36 -0800

    added file

--- PowerShell/Snippets/Create-KeyVault.ps1
+++ PowerShell/Snippets/Create-KeyVault.ps1
@@ -1,1 +1,16 @@
 #Create new KeyVault with PowerShell
+
+$KeyVault = @{
+    Name = "myKeyVaultName"
+    ResourceGroupName = "myResourceGroupName"
+    Location = "myLocation"
+    EnabledForDeployment = $false
+    EnabledForTemplateDeployment = $false
+    EnabledForDiskEncryption = $false
+    tags = @{
+        tag1 = "value1"
+        tag2 = "value2"
+    }
+}
+
+New-AzKeyVault @KeyVault
