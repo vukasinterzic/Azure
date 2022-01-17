b7f14151d93c57261a7c9d0aaa7b5afab5896bbd
vukasinterzic <vukasin.terzic@outlook.com>
2022-01-17 15:44:50 -0800

    file added

--- PowerShell/Snippets/Create-ResourceGroup.ps1
+++ PowerShell/Snippets/Create-ResourceGroup.ps1
@@ -1,1 +1,12 @@
 #Create a new Resource Group with PowerShell
+
+$RG = @{
+    Name = "MyResourceGroup"
+    Location = "mylocation"
+    Tags = @{
+        "Key1" = "Value"
+        "Key2" = "Value"
+    }
+}
+
+New-AzResourceGroup @RG
