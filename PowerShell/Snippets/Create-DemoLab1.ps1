fefcf3773b8cdc11bde6e79b69823c7492d469d3
vukasinterzic <vukasin.terzic@outlook.com>
2022-01-16 20:54:08 -0800

    work in progress

--- PowerShell/Snippets/Create-DemoLab1.ps1
+++ PowerShell/Snippets/Create-DemoLab1.ps1
@@ -11,7 +11,7 @@
 
 $Number = Get-Random -Minimum 1 -Maximum 254
 $Location = "westus2"
-$ResourceGroupName = "RG-DemoLab$Number"
+$ResourceGroupName = "RG-Test-DemoLab$Number"
 $VirtualNetworkName = "vNet-DemoLab$Number"
 $SubnetName = "subnet1-DemoLab$Number"
 $PIP1Name = "pip-VM1-DemoLab$Number"
@@ -19,43 +19,87 @@ $PIP2Name = "pip-VM2-DemoLab$Number"
 $VM1Name = "VM1-DemoLab$Number"
 $VM2Name = "VM2-DemoLab$Number"
 $VMSize = "Standard_B1ms"
-$VMOS = ""
+#$VMOS = ""
 $VNetAddressPrefix = "10.0.$Number.0/24"
 $SubnetAddressPrefix = "10.0.$Number.0/27"
 $VMUser = "labuser"
 $VMPassword = "Hello$Number$Number!!"
 
-$Tags = $VM.Tags
-$Tags += @{"Environment"="Demo"}
+$Tags = @{
+    Environment = "Demo"
+    Project="BlogPost"
+}
 
 #Create a Resource Group
 Write-Host -ForegroundColor Black -BackgroundColor Cyan "Creating Resource Group $ResourceGroupName ..."
 
-New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Tag $Tags
+$RG = @{
+    Name = $ResourceGroupName
+    Location = $Location
+    Tags = $Tags
+}
 
+New-AzResourceGroup @RG
 
 
 #Create a Virtual Network
-Write-Host -ForegroundColor Black -BackgroundColor Cyan "Creating Virtual Network $VirtualNetworkName ..."
+Write-Host -ForegroundColor Black -BackgroundColor Cyan "Creating Virtual Network $VirtualNetworkName with network space $VNetAddressPrefix and subnet $SubnetName ($SubnetAddressPrefix) ..."
 
-New-AzVNet -Name $VirtualNetworkName -ResourceGroupName $ResourceGroupName -Location $Location -Tag $Tags
+$VNET = @{
+    Name = $VirtualNetworkName
+    Location = $Location
+    ResourceGroupName = $ResourceGroupName
+    AddressPrefix = @($VNetAddressPrefix)
+    
+    Subnet = @{
+        Name = $SubnetName
+        AddressPrefix = $SubnetAddressPrefix
+    }
+}
 
-
-#Create a Subnet
-Write-Host -ForegroundColor Black -BackgroundColor Cyan "Creating Subnet $SubnetName ..."
+New-AzVirtualNetwork @vnet
 
 
 #Create Public IP Addresses
 
 Write-Host -ForegroundColor Black -BackgroundColor Cyan "Creating Public IP Address $PIP1Name ..."
 
+$publicIp1 = @{
+    Name = $PIP1Name
+    ResourceGroupName = $ResourceGroupName
+    AllocationMethod = "Dynamic"
+    Location = $Location
+    Sku = "Basic"
+    Tag = @{
+        VMName = $VM1Name
+    }
+}
+
+New-AzPublicIpAddress @publicIp1
+
 
 Write-Host -ForegroundColor Black -BackgroundColor Cyan "Creating Public IP Address $PIP2Name ..."
+
+$publicIp2 = @{
+    Name = $PIP2Name
+    ResourceGroupName = $ResourceGroupName
+    AllocationMethod = "Dynamic"
+    Location = $Location
+    Sku = "Basic"
+    Tag = @{
+        VMName = $VM2Name
+    }
+}
+
+New-AzPublicIpAddress @publicIp2
+
+
 
 
 #Create VMs:
 
 Write-Host -ForegroundColor Black -BackgroundColor Cyan "Creating Virtual Machine $VM1Name ..."
+
 
 
 Write-Host -ForegroundColor Black -BackgroundColor Cyan "Creating Virtual Machine $VM2Name ..."
